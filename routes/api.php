b4197993e9484d06ccae04ebb1bebc856e3644ad
Kyle Stevenson <kyle@kylestevenson.me>
2017-11-05 12:02:04 -0800

    Add artifacts route

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,15 @@ Route::get('/packs', function (App\Gamepacks $gamepacks) {
     }));
 });
 
+Route::get('/artifacts', function (App\Gamepacks $gamepacks, App\Deobs $deobs) {
+    return $gamepacks->all()->pluck('rev')
+        ->map(function ($rev) use ($gamepacks, $deobs) {
+            $pack = $gamepacks->find($rev);
+            $deob = $deobs->find($rev);
+            return compact('pack', 'deob');
+        })->keyBy('pack.rev');
+});
+
 Route::get('/artifacts/{rev}', function ($rev, App\Gamepacks $gamepacks, App\Deobs $deobs) {
     $deob = $deobs->find($rev);
     $pack = $gamepacks->find($rev);

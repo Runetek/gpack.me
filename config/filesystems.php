724484fb9ba37f1900697135b0712fee10918d1a
Kyle Stevenson <kyle@kylestevenson.me>
2017-11-05 10:02:38 -0800

    Add basica redirects

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,16 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('MINIO_KEY'),
+            'secret' => env('MINIO_SECRET'),
+            'region' => env('MINIO_REGION'),
+            'bucket' => env('MINIO_BUCKET'),
+            'endpoint' => env('MINIO_HOST'),
+            'use_path_style_endpoint' => true,
+        ],
+
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/07/{rev}', function ($rev) {
+    $s3 = Storage::cloud();
+    $filename = sprintf('%s/gamepack.jar', $rev);
+
+    abort_unless($s3->exists($filename), 404, 'Not found');
+
+    return redirect($s3->url(sprintf('%s/gamepack.jar', $rev)));
+});

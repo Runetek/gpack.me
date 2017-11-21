1f0cd49769405bf6e32b790132ec79f73a79d8ef
Kyle Stevenson <kyle@kylestevenson.me>
2017-11-21 15:18:56 -0800

    Add url to reports

--- app/Http/Resources/Report.php
+++ app/Http/Resources/Report.php
@@ -3,9 +3,17 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Contracts\Filesystem\Cloud;
+use Illuminate\Support\Facades\Storage;
 
 class Report extends Resource
 {
+    public function __construct($resource)
+    {
+        parent::__construct($resource);
+        $this->s3 = Storage::disk('reports');
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -17,6 +25,9 @@ class Report extends Resource
         return [
             'revision' => (int) $this->revision,
             'fqcn' => $this->reportType->fqcn,
+            'url' => $this->s3->url(
+                sprintf('%d/%s.txt', $this->revision, $this->reportType->fqcn)
+            ),
         ];
     }
 }

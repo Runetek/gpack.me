7ed46807908a66ebfb126d64c10fa8ac66b1311f
Kyle Stevenson <kyle@kylestevenson.me>
2017-11-21 01:46:00 -0800

    Add v2 api namespace

--- routes/api.php
+++ routes/api.php
@@ -5,26 +5,29 @@ use App\Http\Resources\Report as ReportResource;
 use Illuminate\Pagination\LengthAwarePaginator;
 use App\Report;
 
-Route::get('reports', function () {
-    return ReportResource::collection(
-        Report::with('reportType')
-            ->orderBy('revision', 'desc')
-            ->paginate(25)
-    );
-})->name('reports');
+Route::group(['prefix' => 'v2'], function () {
+    Route::get('reports', function () {
+        return ReportResource::collection(
+            Report::with('reportType')
+                ->orderBy('revision', 'desc')
+                ->paginate(25)
+        );
+    })->name('reports');
 
-Route::get('reports/{revision}', function ($revision, ReportFetcher $reports) {
-    return $reports->all()
-        ->where('revision', '=', $revision)
-        ->values();
+    Route::get('reports/{revision}', function ($revision, ReportFetcher $reports) {
+        return $reports->all()
+            ->where('revision', '=', $revision)
+            ->values();
+    });
+
+    Route::get('search_index', function (ReportFetcher $reports) {
+        return [
+            'revisions' => $reports->availableRevisions(),
+            'reportTypes' => $reports->availableReportTypes(),
+        ];
+    });
 });
 
-Route::get('search_index', function (ReportFetcher $reports) {
-    return [
-        'revisions' => $reports->availableRevisions(),
-        'reportTypes' => $reports->availableReportTypes(),
-    ];
-});
 
 Route::get('/packs', function (App\Gamepacks $gamepacks) {
     return response()->json($gamepacks->all()->map(function ($pack) use ($gamepacks) {

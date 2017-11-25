58fbbdd79275f0c5db0c497af809e95e8ea0a0a4
Kyle Stevenson <kyle@kylestevenson.me>
2017-11-25 01:48:20 -0800

    Add vuetable-2

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import { VueTyper } from 'vue-typer'
+import VueTable from 'vuetable-2'
 
 require('./bootstrap')
 
 window.Vue = require('vue')
 
+Vue.use(VueTable)
+
 Vue.component('vue-typer', VueTyper)
-Vue.component('example-component', require('./components/ExampleComponent.vue'))
+// Vue.component('example-component', require('./components/ExampleComponent.vue'))
 
 const app = new Vue({
     el: '#app'

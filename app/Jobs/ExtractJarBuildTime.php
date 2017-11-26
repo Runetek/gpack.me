d4ff5f5ea2ca10d9efe863a392ad633e14148e05
Kyle Stevenson <kyle@kylestevenson.me>
2017-11-25 23:16:21 -0800

    Queue build time extraction

--- app/Jobs/ExtractJarBuildTime.php
+++ app/Jobs/ExtractJarBuildTime.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Spatie\MediaLibrary\Media;
 
-class ExtractJarBuildTime //implements ShouldQueue
+class ExtractJarBuildTime implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
